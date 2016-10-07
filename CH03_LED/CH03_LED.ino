bdc1d57c39a6603cd928f0e735bc1d817103b6d8
Allen Yan <Allen@Allens-MacBook-Pro-2.local>
2016-10-07 17:50:07 -0400

    Challenge3 for pi test

---
+++ CH03_LED/CH03_LED.ino
@@ -0,0 +1,100 @@
+int val5 = 0;
+int val6 = 0;
+int val7 = 0;
+int val8 = 0;
+int val13 = 0;
+String first  = Serial.readStringUntil(',');
+int LED_Nu = first.toInt();
+int second = Serial.readStringUntil('.').toInt();
+void setup() {
+  // initialize serial communication:
+  Serial.begin(9600);
+  pinMode(0, OUTPUT);
+  pinMode(1, OUTPUT);
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+  pinMode(12, OUTPUT);
+  pinMode(13, OUTPUT);
+val5 = digitalRead(5);   // read the input pin
+val6 = digitalRead(6);
+val7 = digitalRead(7);
+val8 = digitalRead(8);
+val13 = digitalRead(13);
+Serial.println(val5+val6+val7+val8+val13);
+}
+
+void loop() {
+  
+
+
+if( Serial.available()>0)
+{
+
+    
+    //Serial.read(); //next character is comma, so skip it using this
+    //Serial.println(LED_Nu);
+    //Serial.println(second);
+  switch (LED_Nu) {
+    
+     case 5:   
+      if (second == 0)
+       {digitalWrite(5, LOW);
+       Serial.println("the value is 0 of port 5");
+       break;
+       }else if (second ==1)
+       digitalWrite(5, HIGH);
+       Serial.println("the value is 1 of port 5");
+     break;
+      
+    case  6: 
+     if (second == 0)
+       {digitalWrite(6, LOW);
+       Serial.println("the value is 0 of port 6");
+       break;}
+       
+       else if (second ==1)
+       digitalWrite(6, HIGH);
+       Serial.println("the value is 1 of port 6");
+       break;
+    
+    case  7: 
+        if (second == 0)
+       {digitalWrite(7, LOW);
+       Serial.println("the value is 0 of port 7");
+       break;
+       }else if (second ==1)
+       digitalWrite(7, HIGH);
+       Serial.println("the value is 1 of port 7");
+       break;
+    
+    case  8: 
+        if (second == 0)
+       {digitalWrite(8, LOW);
+       Serial.println("the value is 0 of port 8");
+       break;}
+       else if (second ==1)
+       digitalWrite(8, HIGH);
+       Serial.println("the value is 1 of port 8");
+       break;
+
+     case  13: 
+        if (second == 0)
+       {digitalWrite(13, LOW);
+       Serial.println("the value is 0 of port 13");
+       break;}
+       else if (second ==1)
+       digitalWrite(13, HIGH);
+       Serial.println("the value is 1 of port 13");
+       break;
+  }
+  delay(1000); 
+}       
+}
---
+++ CH03_LED/server.js
@@ -0,0 +1,106 @@
+var SerialPort = require("serialport");	//communicate with Xbee coordinator
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);		// use for real-time update
+
+function getDateTime() {			// use for last sent update
+    var curt     = new Date();
+    var hour    = curt.getHours();
+    var minute  = curt.getMinutes();
+    var second  = curt.getSeconds();
+    if (hour.toString().length == 1) {
+        var hour = '0'+hour;
+    }
+    if (minute.toString().length == 1) {
+        var minute = '0'+minute;
+    }
+    if (second.toString().length == 1) {
+        var second = '0'+second;
+    }
+    var time = hour+':'+minute+':'+second;
+     return time;
+}
+
+var portName = process.argv[2],
+portConfig = {
+	baudRate: 9600,
+	parser: SerialPort.parsers.readline("\n")
+};
+
+var sp;
+var led = 0;
+sp = new SerialPort.SerialPort(portName, portConfig);
+sp.on("open", function () {
+    console.log('Serial port open');
+    /*setInterval(function(){
+
+    	    sp.write('5,0.');
+
+
+    }, 2000);
+    setInterval(function(){
+
+    	    sp.write('5,1.');
+
+
+    }, 2000);*/
+
+ /*   setTimeout(function(){
+	sp.write('5,1.');
+	sp.write('6,1.');
+	sp.write('7,1.');
+	sp.write('8,1.');
+	sp.write('13,1.');
+    },2000); */
+    setInterval(function(){
+	if (led == 0) {
+	    sp.write('5,0.');
+	    led = 1;
+	}
+	else {
+	    sp.write('5,1.');
+	    led = 0;
+	}
+}, 2000);
+/*setInterval(function(){
+if (led == 0) {
+  sp.write('5,0.');
+  led = 1;
+}
+else {
+  sp.write('5,1.');
+  led = 0;
+}
+}, 2000);*/
+  sp.on('data', function(data) {
+      // sanitize input data
+      if (data.length==7) {
+	  console.log('status: ' + data);
+	  // send data on to website
+	  io.emit("data", {status:data, time:getDateTime()});
+      }
+  });
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
+// Webpage with LED status
+app.get('/', function(req, res){
+    res.sendFile('index.html' , { root : __dirname});
+});
+//update
+app.get('/update', function(req, res){
+    //extract command from req
+    var cmd = req.something;
+    //send to serial port
+    sp.write(data, function () {
+	sp.drain(console.log('sent command: ' + cmd));
+  });
+    //don't send anything back to website
+});
