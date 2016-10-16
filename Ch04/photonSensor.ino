63f233d3f8249d6a53319dddb07b377e9be6473f
Prerana Haridoss <preranah@bu.edu>
2016-10-16 11:21:48 -0400

    added sensor code for ch4

---
+++ Ch04/photonSensor.ino
@@ -0,0 +1,36 @@
+#include <math.h>
+
+const int PULLUP_RES = 90000; // in Ohm( 20kOm )
+
+const double BETA = 4390; // in K for Semitec 104 GTA-2
+
+const double THERMISTOR_RES = 100000; // in Ohm
+
+const double THERMISTOR_NOM_TEMP = 23; // Celsius, C
+
+void setup()
+{
+}
+
+void loop()
+{
+ thermister_temp(analogRead(4));
+ delay(2000);
+}
+
+void thermister_temp(int aval)
+{
+ double R, T;
+
+R = (double) PULLUP_RES / ( (4095 / (double) aval ) - 1 );
+ 
+ T = 1 / ( ( 1 / (THERMISTOR_NOM_TEMP + 273.15 )) + ( ( 1 / BETA) * log ( R / THERMISTOR_RES ) ) );
+ 
+ T -= 273.15; // converting to C from K
+ 
+ // return degrees C
+ Spark.publish("Temperature", String(T) + " °C");
+
+}
+
+
