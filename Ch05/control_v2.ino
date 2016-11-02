ec8c083dd588f4cad69e61ca7d030e863376f6cc
Mohammed <root@dhcp-wifi-8021x-155-41-38-82.bu.edu>
2016-11-02 15:18:45 -0400

    latest update

--- Ch05/control_v2.ino
+++ Ch05/control_v2.ino
@@ -1,270 +1,276 @@
-/*
- * Traking all used PIN's
- * servo  PIN 5
- * ESC    PIN 9
- * lidar PIN 2,3, A4,A5
- * IR PIN A0, A1
- * Xbee PIN ?
- * 
- */
-
-#include <Wire.h>   // for LIDAR
+#include <MsTimer2.h>
 #include <Servo.h>
 #include <math.h>
-#define    INTERVAL     50      // (ms) Interval between distance readings.
-//#define    BOUNDARY     42      // (cm) Avoid objects closer than 30cm.
-#define    LIDARLite_ADDRESS   0x62       // Default I2C Address of LIDAR-Lite - party line
-#define    RegisterMeasure     0x00       // Register to write to initiate ranging.
-#define    MeasureValue        0x04       // Value to initiate ranging.
-#define    RegisterHighLowB    0x8f       // Register to get both High and Low bytes in 1 call.
-#define    LeftRangeSensor     2          // Pow-En Pin for LIDAR mounted on device left.
-#define    RightRangeSensor    3          // Pow-En Pin for LIDAR mounted on device right.
+
+#define INTERVAL      10      // (ms) Interval between distance readings.
+#define BOUNDARY      80      // Avoid objects closer than 30cm.
+#define IR_THRESHOLD  100     // Max acceptable reading of front collision sensor
+#define ESC_STOP      90      // Neutral ("Stop") value for ESC
+
+// Tracking all used pins
+#define LeftRangeTrigger      3   // Trigger Pin for LIDAR mounted on device left.
+#define LeftRangeSensor       7  // Monitor Pin for LIDAR mounted on device left.
+#define RightRangeTrigger     4   // Trigger Pin for LIDAR mounted on device right.
+#define RightRangeSensor      6   // Monitor Pin for LIDAR mounted on device right.
+#define FrontCollisionSensor  A0  // Front SharpIR sensor
+#define BackCollisionSensor   A1  // Back SharpIR sensor
+#define ServoPin              9   // Servo to control wheel turn (PWM)
+#define EscPin                5   // Speed control (PWM)
+
 Servo myservo;
-Servo esc; //ESC can be controlled like a servo.
-int   BOUNDARY = 42;      // (cm) Avoid objects closer than 30cm.
-String stop_start;
+Servo esc;              // ESC can be controlled like a servo.
+
+// Global variables
+char stop_start;        // Store latest xbee command
+int safety_check;       // Store latest collision sensor reading
+// LIDAR
 int distance_from_obstacle_1 = 0; // distance from the wall 1
 int distance_from_obstacle_2 = 0; // distance from the wall 2
+int min_distance_to_wall ;        // keep track of the min distance
+// Servo
 int pos = 0;                      // Position of the servo (degress, [0, 180])
-int min_distance_to_wall ;           // keep track of the min distances this is can be modified later
-double maxSpeedOffset = 45; // maximum speed magnitude, in servo 'degrees'
-double maxWheelOffset = 85; // maximum wheel turn magnitude, in servo 'degrees'
-int duration;
-String wheel; // this is used to know which wheel is closer to the wall used string are "right_wheel" or "left_wheel" 
-unsigned long pulse_widthleft;
-unsigned long pulse_widthright;
-// set the value of IR sensor
+int maxWheelOffset = 85; // maximum wheel turn magnitude, in servo 'degrees'
+int wheelNeutral = 90;      // neutral wheel position, in servo 'degrees'
+int currentTurnDegree = 0; // start with wheels in neutral position
+int angleAcc = 40;         // rate of change of turn
+//ESC
+int maxSpeedOffset = 60; // maximum speed magnitude, in servo 'degrees'
+int minSpeedOffset = 80; // minimum speed magnitude, in servo 'degrees'
+int currentSpeedOffset = ESC_STOP; // start with speed 0
+int linearAcc = 4; // Rate of speed change
 
-int distance = 0;    // Distance measured
+String wheel; // Store which wheel is closer to a wall, used strings are "right_wheel" or "left_wheel"
+unsigned long pulse_width;
 
-void lidarSetup() 
+void irSetup() // Set up signal pins
 {
-  Wire.begin(); // join i2c bus
-  pinMode(LeftRangeSensor, OUTPUT); 
-  pinMode(RightRangeSensor, OUTPUT);
-  delay(2);
-  //Set both sensors to sleep
-  digitalWrite(LeftRangeSensor, LOW);
-  digitalWrite(RightRangeSensor, LOW);
+  pinMode(FrontCollisionSensor, INPUT);
+  pinMode(BackCollisionSensor, INPUT);
+}
+
+int irGetDistance(char sensorId) // Get a measurement from the Sharp IR sensor
+{
+  int reading = 0;
+  if (sensorId == FrontCollisionSensor)
+  {
+    reading = analogRead(FrontCollisionSensor);
+    Serial.print("Reading from Front IR - ");
+    Serial.println(reading);
+  }
+  else if (sensorId == BackCollisionSensor)
+  {
+    reading = analogRead(BackCollisionSensor);
+    Serial.print("Reading from Back IR - ");
+    Serial.println(reading);
+  }
+  else
+  {
+    Serial.print("Invalid input to irGetDistance");
+  }
+  return reading;
+}
+
+void lidarSetup()   // Set up trigger and monitor pins, set trigger pins to low for continuous read
+{
+  pinMode(LeftRangeTrigger, OUTPUT);
+  pinMode(LeftRangeSensor, INPUT);
+  digitalWrite(LeftRangeTrigger, LOW);
+  pinMode(RightRangeTrigger, OUTPUT);
+  pinMode(RightRangeSensor, INPUT);
+  digitalWrite(RightRangeTrigger, LOW);
 }
 
 int lidarGetDistance(int sensorNumber)   // Get a measurement from the LIDAR Lite
 {
-  if (sensorNumber != LeftRangeSensor && sensorNumber != RightRangeSensor) {   // Input sanity check
+  if (sensorNumber != LeftRangeSensor && sensorNumber != RightRangeSensor) // Input sanity check
+  {
     Serial.println("Invalid input to lidarGetDistance");
     return -1;
   }
-      
-  digitalWrite(sensorNumber, HIGH);   // Wake up sensor
-  int val = -1;
-  
-  Wire.beginTransmission((int)LIDARLite_ADDRESS); // transmit to LIDAR-Lite
-  Wire.write((int)RegisterMeasure); // sets register pointer to  (0x00)  
-  Wire.write((int)MeasureValue); // sets register pointer to  (0x00)  
-  Wire.endTransmission(); // stop transmitting
-
-  delay(20); // Wait 20ms for transmit
-
-  Wire.beginTransmission((int)LIDARLite_ADDRESS); // transmit to LIDAR-Lite
-  Wire.write((int)RegisterHighLowB); // sets register pointer to (0x8f)
-  Wire.endTransmission(); // stop transmitting
-
-  delay(20); // Wait 20ms for transmit
-  
-  Wire.requestFrom((int)LIDARLite_ADDRESS, 2); // request 2 bytes from LIDAR-Lite
-
-  if(2 <= Wire.available()) // if two bytes were received
+  pulse_width = pulseIn(sensorNumber, HIGH);  // Count how long the pulse is high in microseconds
+  if (pulse_width != 0)                       // If non-zero reading, proceed
   {
-    val = Wire.read(); // receive high byte (overwrites previous reading)
-    val = val << 8; // shift high byte to be high 8 bits
-    val |= Wire.read(); // receive low byte as lower 8 bits
+    pulse_width = pulse_width / 10;           // 10usec = 1 cm of distance for LIDAR-Lite
+    if (sensorNumber == LeftRangeSensor) Serial.print("left:");
+    else Serial.print("right:");
+    Serial.println(pulse_width);              // Print the distance
+    return pulse_width;
+    delay(1);
   }
-  
-  digitalWrite(sensorNumber, LOW);   // Sleep mode for sensor
-
-  // Add code here for distance corrections after sensor mounted and calibrated
-  // For now, subtracting offset assuming sensors mounted in the middle of the chassis
-  val = val - 20;
-  return val;
 }
 
-void lidarPrintDistance(int sensorNumber, int distance)
+void calibrate_myservo()
 {
-    if (sensorNumber == LeftRangeSensor) 
-    {
-      Serial.print("Reading from Left LIDAR - ");
-      Serial.print("\t\t\t\tDistance (cm): ");
-      Serial.println(distance);
-    }
-    else if (sensorNumber == RightRangeSensor) 
-    {
-      Serial.print("Reading from Right LIDAR - ");
-      Serial.print("\tDistance (cm): ");
-      Serial.println(distance);
-    }
-    else {
-      Serial.println("Invalid input to lidarPrintDistance");
-    }
+  Serial.println("<===waiting for Xbee signal to start===>");
+  while ( stop_start == '0') // while there is No Xbee signal to start MTA:
+  {
+    esc.write(ESC_STOP);                  // reset the ESC to neutral (non-moving) value
+    pos = analogRead(myservo.attach(ServoPin));  // reads the value of the potentiometer (value between 0 and 1023)
+    pos = map(pos, 0, 1023, 0, 179);      // scale it to use it with the servo (value between 0 and 180)
+    myservo.write(wheelNeutral);                    // sets the servo position according to the scaled value
+    Serial.println("inside the calibrate_mode");
+
+    // getting both distances
+    distance_from_obstacle_1 = lidarGetDistance(LeftRangeSensor);
+    distance_from_obstacle_2 = lidarGetDistance(RightRangeSensor);
+    min_distance_to_wall = min (distance_from_obstacle_1, distance_from_obstacle_2);
+
+    Serial.println("min_distance_to_wall");
+    Serial.println(min_distance_to_wall);
+    delay(1000);
+  }
 }
 
+void timerSetup() {
+  MsTimer2::set(1000, check); // set(period in ms, function name)
+  MsTimer2::start();
+}
+
+void check() {  // Interrupt to get xbee and collision sensor updates
+  Serial.println("X-bee check");
+  if (Serial.available() > 0)
+  {
+    stop_start = Serial.read();
+    Serial.println("Received update from x-bee");
+    Serial.println(stop_start);
+  }
+  Serial.println("Collision sensor check");
+  safety_check = irGetDistance(FrontCollisionSensor);
+  Serial.println(safety_check);
+}
 
 void setup()
 {
   Serial.begin(9600);
-   stop_start = "0"; //stop
-  Serial.println("< calibration Mode started waite for 3 sec >");
+  stop_start = '0'; //stop
+  Serial.println("< Calibration Mode started wait for 3 sec >");
+  irSetup();
+  timerSetup();
   lidarSetup();
-  myservo.attach(5);   // Servo control use pin 5 for PWM to servo
-  esc.attach(9); // ESC control use pin 9 for PWM to ESC
-  calibrate_myservo(); // making sure that the servo is in the right position
-  
-
+  myservo.attach(ServoPin);   // Servo setup
+  esc.attach(EscPin);         // ESC setup
+  calibrate_myservo();        // make sure that the servo is in the right position
 }
+
 /* Convert degree value to radians */
-double degToRad(double degrees){
+double degToRad(double degrees) {
   return (degrees * 71) / 4068;
 }
 
 /* Convert radian value to degrees */
-double radToDeg(double radians){
+double radToDeg(double radians) {
   return (radians * 4068) / 71;
 }
 
 void forward()
 {
-  myservo.write(90);
-  esc.write(60);
-  delay(100);
+/*  //straighten out
+  if (currentTurnDegree != 0)
+  {
+    if (currentTurnDegree < 0) currentTurnDegree = currentTurnDegree + angleAcc;
+    else currentTurnDegree = currentTurnDegree - angleAcc;
+    turn();
+  } 
+  */
+  currentTurnDegree=5;
+  turn();
+  //speed up 
+  if (currentSpeedOffset > maxSpeedOffset) {
+    currentSpeedOffset = currentSpeedOffset - linearAcc;
+    esc.write(currentSpeedOffset);
+  } 
+  delay(INTERVAL);
   Serial.println("forward Mode");
 }
 
-void leftTurn(int turnDegree)
+void turn()
+{
+  // reduce the speed
+  if (currentSpeedOffset < minSpeedOffset) {
+    currentSpeedOffset++;
+    esc.write(currentSpeedOffset);
+  }
+  //double rad = degToRad(currentTurnDegree);
+  //double wheelOffset = sin(rad) * maxWheelOffset;
+  //myservo.write(wheelNeutral + (int)wheelOffset); // update the servo
+  int wheelOffset = currentTurnDegree;
+  myservo.write(wheelNeutral + wheelOffset); // update the servo
+  if (wheelOffset > 0)
   {
-    esc.write(70); // reduce the speed
-    myservo.write(90 + turnDegree); // update tge servo
     Serial.println("leftTurn Mode");
-    delay(duration);
   }
-
-void rightTurn(int turnDegree)
+  else if (wheelOffset < 0)
   {
-    esc.write(70);// reduce the speed
-    myservo.write(90 + turnDegree); // update tge servo
     Serial.println("rightTurn Mode");
-    delay(duration);
   }
-void calibrate_myservo(){
+  delay(150);
+}
 
-  Serial.println("<===waiting for Xbee signal to start===>");
-  
-while ( Serial.available()==0) // while there is No Xbee signal to start MTA:
+void track_wall()  // used if the car far from the walls
+{
+  distance_from_obstacle_1 = lidarGetDistance(LeftRangeSensor);
+  distance_from_obstacle_2 = lidarGetDistance(RightRangeSensor);
+  min_distance_to_wall = min(distance_from_obstacle_1, distance_from_obstacle_2); // keep track of the min distances and store that in the array
+
+  Serial.println(min_distance_to_wall);       // Print it out.
+  if (distance_from_obstacle_1 < distance_from_obstacle_2) // keep track of the closer wheel to the wall
   {
-    esc.write(90); // reset the ESC to neutral (non-moving) value
-    pos = analogRead(myservo.attach(5));  // reads the value of the potentiometer (value between 0 and 1023) 
-    pos = map(pos, 0, 1023, 0, 179);     // scale it to use it with the servo (value between 0 and 180) 
-    myservo.write(90);                  // sets the servo position according to the scaled value 
-    Serial.println("inside the calibrate_mode");
-    
-    // getting the both distances 
-    distance_from_obstacle_1 = 50;//lidarGetDistance(LeftRangeSensor);
-    distance_from_obstacle_2 = 50;//lidarGetDistance(RightRangeSensor);
-    min_distance_to_wall = min (distance_from_obstacle_1,distance_from_obstacle_2); // keep track of the min distances and store that in the array 
-    
-    Serial.println("min_distance_to_wall"); 
-    Serial.println(min_distance_to_wall);
-    delay(1000);
+    wheel = "left_wheel";
   }
+  else
+  {
+    wheel = "right_wheel";
+  }
+  delay(45);
+}
 
-  if ( Serial.available()>0)
-  {stop_start = "1";}
-  delay (100);
+void swerve()    // used if the car is too close to a wall and should turn away from the wall
+{
+  Serial.println("Swerving");
+  track_wall();
+  if (wheel == "left_wheel")
+  {
+    if (currentTurnDegree > -90) {
+      currentTurnDegree = currentTurnDegree - angleAcc;
+      Serial.println("close to left wall");
+      turn();   // turn right
+    }
+  }
+  else if (wheel == "right_wheel")
+  {
+    if (currentTurnDegree < 90) {
+      currentTurnDegree = currentTurnDegree + angleAcc;
+      Serial.println("close to right wall");
+      turn();      // turn left
+    }
+  }
+  delay(INTERVAL);                // Delay between readings.
 }
 
 void loop()
 {
- int ii=0;  //local conter for the min distances array
- forward(); // car moves forward continuously.
+  //int turn_angle = 0;       // local counter for turn degree
+  forward();                // car moves forward continuously
+  while (stop_start == '1' && safety_check <= IR_THRESHOLD && min_distance_to_wall >= BOUNDARY) // if far enough from both walls, keep going
+  {
+    forward();
+    track_wall();
+  }
+  if (stop_start == '1' && safety_check <= IR_THRESHOLD && min_distance_to_wall < BOUNDARY) // if too close to a wall, turn away from wall
+  {
+    swerve();
  
- // loop note : this loop will be used if the car far from the wall
- 
-  do 
-
-   {         
-       distance_from_obstacle_1 = 50;//lidarGetDistance(LeftRangeSensor);
-       distance_from_obstacle_2 = 50;//lidarGetDistance(RightRangeSensor);
-       min_distance_to_wall = min (distance_from_obstacle_1,distance_from_obstacle_2); // keep track of the min distances and store that in the array 
-       
-       Serial.println(min_distance_to_wall);       // Print it out.
-    if (distance_from_obstacle_1 < distance_from_obstacle_2) // keep track of the closer wheel to the wall
-      {wheel = "left_wheel";}
-      else
-      {wheel = "right_wheel";}
-        
-//      if (ii == 10) // if the counter reached the max reset it and copy the last read min distance 
-//          { 
-//           // min_distance_to_wall[0] =  min_distance_to_wall[10];
-//          ii = 1;
-//          }
-//
-     delay(1000);
-     if (Serial.available()>0)
-       {stop_start = Serial.readString();}
-       
-//       ii++;
-    }
- while(min_distance_to_wall >= BOUNDARY &&  stop_start == "1");//loop til an object is sensed MTA:
-
-//=======================================================================
-
-//This loop will be used if the car is closer to the right wall then the car should trun left
-// or 
-// This loop will be used if the car is closer to the left wall then the car should trun right
-  
- while(min_distance_to_wall < BOUNDARY && stop_start == "1")
-   { 
-            Serial.println("Test Loop2");       // Print it out.
-
-            double rad = degToRad(ii);
-            double wheelOffset = sin(rad) * maxWheelOffset;
-            Serial.println(BOUNDARY);
-            if (wheel == "left_wheel")
-            {Serial.println("close to left wall"); 
-            
-            rightTurn((-1)*wheelOffset); // calling trun right function
-            ii++; //degree counter
-            Serial.println("calling rightTurn() function");
-            delay(INTERVAL);                // Delay between readings.
-            }
-            
-            else if (wheel == "right_wheel")
-            {Serial.println("closer to right wall"); 
-            //Serial.println(min_distance_to_wall); 
-            leftTurn(wheelOffset); // calling trun right function
-            ii++; //degree counter
-            delay(INTERVAL);                // Delay between readings.
-            Serial.println("calling rightTurn() function");
-            }
-            if (Serial.available()>0)
-       {stop_start = Serial.readString();
-       //min_distance_to_wall = 50;//lidarGetDistance(LeftRangeSensor);
-
-       }
-                             
-    }
- //======================================================================
- //??(should be in the loop of turn) to make sure the car is going straight forward() function is called in the next loop to recover the added/subtracted angles
- // when read stop from Xbee, stop
- do{
-    esc.write(90); // MTA: 90 means stop
-    if (Serial.available()>0)
-       {stop_start = Serial.readString();
-       break;
-     }   
-   }
- while( stop_start == "0");
- //when the IR sensor read something, stop
- //do{
-  //esc.write(90);
- //}
- //while(true); 
+  }
+  while (stop_start == '0')
+  {
+    esc.write(ESC_STOP);    // abrupt stop
+    Serial.println("Stopped because of x-bee command");
+    delay(200);
+  }
+  while (safety_check > IR_THRESHOLD)
+  {
+    esc.write(ESC_STOP);    // abrupt stop
+    Serial.println("Stopped to avoid imminent collision");
+    delay(200);
+  }
 }
