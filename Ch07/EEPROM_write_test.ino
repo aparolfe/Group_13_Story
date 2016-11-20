7e9b4394d82c44e80e0055c3b8b8581f4a3bd861
aparolfe <aparolfe@users.noreply.github.com>
2016-11-20 13:16:44 -0500

    added source of EEPROM read/write program

--- Ch07/EEPROM_write_test.ino
+++ Ch07/EEPROM_write_test.ino
@@ -1,26 +1,23 @@
+// Code adapted from http://forum.arduino.cc/index.php?topic=45104.0
 
-// WRITES TO SERIAL EEPROM Arduino
-//
-// Do this only eleven on an Arduino,
-// Write the Serial of the Arduino in the
-// First 6 bytes of the EEPROM
+// Write the Serial of the Arduino in the first 2 bytes of the EEPROM
 
 #include <EEPROM.h>
-char sID[7] = "A5";
+char sID[3] = "A5";
 
 void setup ()
 {
- Serial.begin (9600);
- for (int i=0; i<2; i++) {
-   EEPROM.write(i,sID[i]);
- }
- Serial.println("Written"); 
- Serial.println(sID); 
- for (int i=0; i<2; i++) {
-   sID[i] = EEPROM.read(i);
- }
- Serial.println("Read");
- Serial.println(sID);
+  Serial.begin (9600);
+  for (int i=0; i<2; i++) {
+    EEPROM.write(i,sID[i]);
+  }
+  Serial.println("Written"); 
+  Serial.println(sID); 
+  for (int i=0; i<2; i++) {
+    sID[i] = EEPROM.read(i);
+  }
+  Serial.println("Read");
+  Serial.println(sID);
 }
 
 void loop () {
