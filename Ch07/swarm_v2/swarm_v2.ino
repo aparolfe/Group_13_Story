189c34fb60dd78ae3e321c3bac3af2552bef5e8d
Prerana Haridoss <preranah@bu.edu>
2016-11-20 19:25:26 -0500

    funtionality for the loop etc

--- Ch07/swarm_v2/swarm_v2.ino
+++ Ch07/swarm_v2/swarm_v2.ino
@@ -6,95 +6,109 @@ SoftwareSerial xbeeSerial(2, 3);
 ZBRxResponse rxResponse = ZBRxResponse();
 ZBTxRequest txRequest;
 
-const int BLUE_LED=6;    // leader
-const int RED_LED=4;    // infected
-const int GREEN_LED=5;    // clear
-const int PIN_BUTTON=8;     // infection button
+const int button = 8;
+const int BLUE_LED = 6;  // leader
+const int RED_LED = 4;  // infected
+const int GREEN_LED = 5;  // clear
+const int PIN_BUTTON = 8;   // infection button
 
-bool leader = true;
-bool infected = false;
+int buttonState;             // the current reading from the input pin
+int lastButtonState = LOW;   // the previous reading from the input pin
+unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
+unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
+
+
+
+bool isleader = false;
+bool isinfected = false;
 int wait_for_Leader_heart_beat = 4000;
+int heartbeat_period = 5000;
+int infection_period = 2000;
+int immunity_period = 3000;
 
-uint32_t immunityTimeout=millis();
-uint32_t infectionRebroadcastTimeout=millis();
 uint32_t myID = 0xA1;
 
-const uint8_t Massage,
-              IsthereLeader = 0xC1,
-              HeartBeat_Reply = 0xC2,
-              Clear_Infection = 0xC3,
-              Infection_to_all = 0xC3;
+uint8_t message;
+const uint8_t IsthereLeader = 0xC1,
+              HEARTBEAT = 0xC2,
+              CLEAR = 0xC3,
+              INFECTION = 0xC4;
 
-
+uint32_t last_immune = millis();
+uint32_t last_heartbeat = millis();
+uint32_t last_infected = millis();
 
 void setup() {
   Serial.begin(57600);
   xbeeSerial.begin(57600);
   xbee.begin(xbeeSerial);
+  pinMode(button, INPUT);
 
   pinMode(BLUE_LED, OUTPUT);
   pinMode(RED_LED, OUTPUT);
   pinMode(GREEN_LED, OUTPUT);
 
-// Make all LED's on to indicate that we are in the setup mode
+  // Make all LED's on to indicate that we are in the setup mode
   digitalWrite(BLUE_LED, HIGH);
   digitalWrite(RED_LED, HIGH);
   digitalWrite(GREEN_LED, HIGH);
   delay(2000);
 
-  Serial.println(myAddress); // send my address to all
-  
+
+
   sendCommand(0x0000FFFF, (uint8_t*)&  IsthereLeader, 1); // is there any leader in the network
-  
+
   delay(wait_for_Leader_heart_beat); //wait to hear if there is a heart beat from the leader
-  
+
   //if ( there is feedback)
   // I'm not a leader
   //
   // else
-  // 
+  //
   //  go to election()
 
 }
 
 //============ This Function To SET LED's =====================
 
-void set_Leds(bool isleader,bool isinfected) {
-  if (isleader==true  && isinfected == false || isleader==true  && isinfected == true)   {
+void set_leds() {
+  if (isleader == true)   {
     digitalWrite(BLUE_LED, HIGH);
     digitalWrite(GREEN_LED, LOW);
     digitalWrite(RED_LED, LOW);
-  } 
-  else if (isLeader==false && isinfected == true){
-      digitalWrite(BLUE_LED, LOW);
-      digitalWrite(GREEN_LED, LOW);
-      digitalWrite(RED_LED, HIGH);}
-      
-  else if (isLeader==false && isinfected == false)  {
-      digitalWrite(BLUE_LED, LOW);
-      digitalWrite(GREEN_LED, HIGH);
-      digitalWrite(RED_LED, LOW);
-    }
+  }
+  else if (isleader == false && isinfected == true) {
+    digitalWrite(BLUE_LED, LOW);
+    digitalWrite(GREEN_LED, LOW);
+    digitalWrite(RED_LED, HIGH);
   }
 
-//========================== Send API Masseges==================== 
+  else if (isleader == false && isinfected == false)  {
+    digitalWrite(BLUE_LED, LOW);
+    digitalWrite(GREEN_LED, HIGH);
+    digitalWrite(RED_LED, LOW);
+  }
+}
+
+//========================== Send API Masseges====================
 
 void sendCommand(uint32_t destination_Addr, uint8_t* payload, uint8_t length) {
-  
-  if (payload[0] == MSG_DISCOVERY || payload[0] == MSG_VICTORY) {
-    txRequest = ZBTxRequest(XBeeAddress64(0x00000000, 0x0000FFFF), payload, length);
-    xbee.send(txRequest);
-  } else {
-    if (destination_Addr == 0x0000FFFF) {
-      for (int i = 0; i < numDevices; i++) {
-        txRequest = ZBTxRequest(XBeeAddress64(0x0013A200, listAddress64[i]), payload, length);
-        xbee.send(txRequest);
-      }
-    } else {
-      txRequest = ZBTxRequest(XBeeAddress64(0x0013A200, destination_Addr), payload, length);
-      xbee.send(txRequest);
-    }
-  }
+
+  //if (payload[0] == MSG_DISCOVERY || payload[0] == MSG_VICTORY) {
+  txRequest = ZBTxRequest(XBeeAddress64(0x00000000, 0x0000FFFF), payload, length);
+  //Serial.println("SENDING...");
+  xbee.send(txRequest);
+  /* } else {
+     if (destination_Addr == 0x0000FFFF) {
+       for (int i = 0; i < numDevices; i++) {
+         txRequest = ZBTxRequest(XBeeAddress64(0x0013A200, listAddress64[i]), payload, length);
+         xbee.send(txRequest);
+       }
+     } else {
+       txRequest = ZBTxRequest(XBeeAddress64(0x0013A200, destination_Addr), payload, length);
+       xbee.send(txRequest);
+     }
+    } */
 }
 
 //=================== Read from API ==========================
@@ -103,36 +117,88 @@ void sendCommand(uint32_t destination_Addr, uint8_t* payload, uint8_t length) {
 //============================================================
 
 //============= Election Method================
-void Electing(void)
+void elect(void)
 {
-   sendCommand(0x0000FFFF, (uint8_t*) & myID, 1);
+  sendCommand(0x0000FFFF, (uint8_t*) & myID, 1);
 
- }
+}
 //=============================================
-void loop() {
-  
+
+void infect(void) {
+  if (millis() > last_immune + immunity_period){
+    isinfected = true;
+    set_leds();
+    sendCommand(0x0000FFFF, (uint8_t*) & INFECTION, 1);
+    last_infected = millis();
+    } 
+  }
+void disinfect(void) {
+  isinfected = false;
+  set_leds();
+  last_immune = millis();
+  sendCommand(0x0000FFFF, (uint8_t*) & CLEAR, 1);
 }
 
-    xbee.getResponse().getZBRxResponse(rxResponse);
-    Massage = rxResponse.getData(0);
+void loop() {
 
-      if (leaderAddress == myAddress) {
-          set_Leds(isLeader,isInfected);
-          sendCommand(0x0000FFFF, (uint8_t*) & HeartBeat_Reply, 1);
-        if (button_state == LOW) 
-         {
-          sendCommand(0x0000FFFF, (uint8_t*) & Clear_Infection, 1);
-         }
+  if (xbee.readPacket(1) && xbee.getResponse().getApiId() == ZB_RX_RESPONSE) {
+    xbee.getResponse().getZBRxResponse(rxResponse);
+    //remoteAddress64 = rxResponse.getRemoteAddress64().getLsb();
+
+    //serialLog(true, remoteAddress64, rxResponse.getData(0));
+    message = rxResponse.getData(0);
+
+    Serial.println(message);
+    switch (message) {
+      case HEARTBEAT:
+        { Serial.println("HEARTBEAT");
+          if (isleader) elect();
+          else last_heartbeat = millis();
+          break;
         }
-        else if (leaderAddress != myAddress){
-          set_Leds(!isLeader,isInfected);
-             if (button_state == LOW) 
-              {
-                sendCommand(0x0000FFFF, (uint8_t*) & Infection_to_all, 1);
-                set_Leds(!isLeader,isInfected);
-              }
-            }
+      case INFECTION: if(!isleader) infect(); break;
+      case CLEAR: if(!isleader) disinfect(); break;
     }
+  }
+
+  if (isleader && (millis() > last_heartbeat + heartbeat_period)) {
+    sendCommand(0x0000FFFF, (uint8_t*) & HEARTBEAT, 1);
+    last_heartbeat = millis();
+  }
+
+  if (isinfected && (millis() > last_infected + infection_period)) {
+    sendCommand(0x0000FFFF, (uint8_t*) & INFECTION, 1);
+    last_infected = millis();
+  }
+
+  //debouncing
+  int reading = digitalRead(button);
+  if (reading != lastButtonState) {
+    // reset the debouncing timer
+    lastDebounceTime = millis();
+  }
+
+  if ((millis() - lastDebounceTime) > debounceDelay) {
+    // whatever the reading is at, it's been there for longer
+    // than the debounce delay, so take it as the actual current state:
+
+    // if the button state has changed:
+    if (reading != buttonState) {
+      buttonState = reading;
+
+      // only toggle the LED if the new button state is HIGH
+      if (buttonState == LOW) {
+        if (isleader) disinfect();
+        else infect();
+      }
+    }
+  }
+  // save the reading.  Next time through the loop,
+  // it'll be the lastButtonState:
+  lastButtonState = reading;
+}
+
+
 
 
 
