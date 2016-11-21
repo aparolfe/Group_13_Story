7043d4755e7efaf34e35dce77da2dfadee84a1fb
aparolfe <aparolfe@users.noreply.github.com>
2016-11-21 13:50:10 -0500

    election works when leader dies, infection not persistent

--- Ch07/swarm_v2/swarm_v2.ino
+++ Ch07/swarm_v2/swarm_v2.ino
@@ -7,80 +7,60 @@ SoftwareSerial xbeeSerial(2, 3);
 ZBRxResponse rxResponse = ZBRxResponse();
 ZBTxRequest txRequest;
 
-const int BLUE_LED = 6;  // leader
-const int RED_LED = 4;  // infected
+const int BLUE_LED = 6;   // leader
+const int RED_LED = 4;    // infected
 const int GREEN_LED = 5;  // clear
-const int PIN_BUTTON = 8;   // infection button
+const int PIN_BUTTON = 8; // infection button
 
 int buttonState;             // the current reading from the input pin
 int lastButtonState = LOW;   // the previous reading from the input pin
 unsigned long lastDebounceTime = 0;  // the last time the output pin was toggled
 unsigned long debounceDelay = 50;    // the debounce time; increase if the output flickers
 
-
-
-bool isleader = true;
+bool isleader = false;
 bool isinfected = false;
-int wait_for_Leader_heart_beat = 4000;
-int heartbeat_period = 5000;
-int infection_period = 2000;
-int immunity_period = 3000;
-int election_period = 3000;
+
+int heartbeat_timeout = 9999;    // time since last heartbeat before elect trigger
+int immunity_timeout = 3000;     // time of immunity
+int election_timeout = 3000;     // time to wait before deciding self is leader
+
+int heartbeat_period = 5000;    // time between sent heartbeats
+int infection_period = 2000;    // time between sent infection messages
+
 char myID;
 
 uint8_t message;
-const uint8_t IsthereLeader = 0xC1,
-              HEARTBEAT = 0xC2,
-              CLEAR = 0xC3,
-              INFECTION = 0xC4;
+const uint8_t HEARTBEAT = 0xC1,
+              CLEAR = 0xC2,
+              INFECTION = 0xC3;
 
 uint32_t last_immune = millis();
 uint32_t last_heartbeat = millis();
 uint32_t last_infected = millis();
 uint32_t last_election = millis();
 
-
-
 void setup() {
 
   Serial.begin(57600);
   xbeeSerial.begin(57600);
   xbee.begin(xbeeSerial);
-
-  //for (int i = 0; i < 1; i++) {
-    myID = EEPROM.read(0);
- // }
+  myID = EEPROM.read(0);
   Serial.println("Read");
   Serial.println(myID);
 
   pinMode(PIN_BUTTON, INPUT);
-
   pinMode(BLUE_LED, OUTPUT);
   pinMode(RED_LED, OUTPUT);
   pinMode(GREEN_LED, OUTPUT);
 
-  // Make all LED's on to indicate that we are in the setup mode
-  digitalWrite(BLUE_LED, HIGH);
-  digitalWrite(RED_LED, HIGH);
-  digitalWrite(GREEN_LED, HIGH);
-  delay(2000);
-
-
-
-  sendCommand(0x0000FFFF, (uint8_t*)&  IsthereLeader, 1); // is there any leader in the network
-
-  delay(wait_for_Leader_heart_beat); //wait to hear if there is a heart beat from the leader
-
-  //if ( there is feedback)
-  // I'm not a leader
-  //
-  // else
-  //
-  //  go to election()
-
+  /*  // Make all LED's on to indicate that we are in the setup mode
+    digitalWrite(BLUE_LED, HIGH);
+    digitalWrite(RED_LED, HIGH);
+    digitalWrite(GREEN_LED, HIGH); */
+  set_leds();
 }
 
-//============ This Function To SET LED's =====================
+//============ SET LED's =====================
 
 void set_leds() {
   if (isleader == true)   {
@@ -101,7 +81,7 @@ void set_leds() {
   }
 }
 
-//========================== Send API Masseges====================
+//========================== Send API Messages====================
 
 void sendCommand(uint32_t destination_Addr, uint8_t* payload, uint8_t length) {
 
@@ -122,44 +102,47 @@ void sendCommand(uint32_t destination_Addr, uint8_t* payload, uint8_t length) {
     } */
 }
 
-//=================== Read from API ==========================
+//============= Election Method ================
 
-
-//============================================================
-
-//============= Election Method================
 void elect(void)
 {
   last_election = millis();
   sendCommand(0x0000FFFF, (uint8_t*) & myID, 1);
   Serial.println("election");
-  while (millis() < last_election + election_period) {
+  while (millis() < last_election + election_timeout) {
     if (xbee.readPacket(1) && xbee.getResponse().getApiId() == ZB_RX_RESPONSE) {
       xbee.getResponse().getZBRxResponse(rxResponse);
       message = rxResponse.getData(0);
       Serial.println(message);
       Serial.println(myID);
-      if(message >  (int)myID){
+      if (message >  (int)myID) {
         Serial.println("compare");
         isleader = false;
         set_leds();
-        break;
+        delay(election_timeout);
+        return;
       }
     }
-
   }
+  isleader = true;
+  sendCommand(0x0000FFFF, (uint8_t*) & HEARTBEAT, 1);
+  last_heartbeat = millis();
   set_leds();
 }
-//=============================================
+
+//============= Infection Method ================
 
 void infect(void) {
-  if (millis() > last_immune + immunity_period) {
+  if (millis() > last_immune + immunity_timeout) {
     isinfected = true;
     set_leds();
     sendCommand(0x0000FFFF, (uint8_t*) & INFECTION, 1);
     last_infected = millis();
   }
 }
+
+//============= Clear Method ================
+
 void disinfect(void) {
   isinfected = false;
   set_leds();
@@ -167,38 +150,39 @@ void disinfect(void) {
   sendCommand(0x0000FFFF, (uint8_t*) & CLEAR, 1);
 }
 
-void loop() {
+//============= Main Loop ================
 
+void loop() {
+  //Listen and react to messages
   if (xbee.readPacket(1) && xbee.getResponse().getApiId() == ZB_RX_RESPONSE) {
     xbee.getResponse().getZBRxResponse(rxResponse);
     message = rxResponse.getData(0);
-
     Serial.println(message);
     switch (message) {
       case HEARTBEAT:
-        { Serial.println("HEARTBEAT");
-          if (isleader) elect();
+        { if (isleader) { Serial.println("HEARTBEAT ELECTION"); elect(); }
           else last_heartbeat = millis();
           break;
         }
-      case INFECTION: if (!isleader) infect(); break;
-      case CLEAR: if (!isleader) disinfect(); break;
-      default: elect(); break;
+      case INFECTION: if (!isleader && !isinfected) infect(); break;
+      case CLEAR: if (!isleader && isinfected) disinfect(); break;
+        //default: Serial.println("default"); elect(); break;
     }
   }
-  
+  // Trigger election if no heartbeat for too long
+  if (millis() > last_heartbeat + heartbeat_timeout) { last_heartbeat = millis(); Serial.println("Timeout ELECTION"); elect(); }
 
+  // Send messages if appropriate
   if (isleader && (millis() > last_heartbeat + heartbeat_period)) {
     sendCommand(0x0000FFFF, (uint8_t*) & HEARTBEAT, 1);
     last_heartbeat = millis();
   }
-
-  if (isinfected && (millis() > last_infected + infection_period)) {
+/*  if (isinfected && (millis() > last_infected + infection_period)) {
     sendCommand(0x0000FFFF, (uint8_t*) & INFECTION, 1);
     last_infected = millis();
-  }
+  } */
 
-  //debouncing
+  // Handle button press
   int reading = digitalRead(PIN_BUTTON);
   if (reading != lastButtonState) {
     // reset the debouncing timer
@@ -208,12 +192,10 @@ void loop() {
   if ((millis() - lastDebounceTime) > debounceDelay) {
     // whatever the reading is at, it's been there for longer
     // than the debounce delay, so take it as the actual current state:
-
     // if the button state has changed:
     if (reading != buttonState) {
       buttonState = reading;
-
-      // only toggle the LED if the new button state is HIGH
+      // only toggle the LED if the new button state is LOW
       if (buttonState == LOW) {
         if (isleader) disinfect();
         else infect();
